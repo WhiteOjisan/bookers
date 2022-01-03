bd39f07914522034da1bc5c9eab8449e6f384fb4
EC2 Default User <ec2-user@ip-172-31-85-123.ec2.internal>
2022-01-03 07:36:04 +0000

    [ERROR] rspec

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,13 +8,11 @@ class BooksController < ApplicationController
   end
 
   def show
-  end
-
-  def new
-    @book = Book.new
+    @book = Book.find(params[:id])
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       redirect_to '/books'
@@ -24,6 +22,22 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
